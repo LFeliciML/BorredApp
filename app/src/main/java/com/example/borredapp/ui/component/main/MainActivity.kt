a9fe1f98b01b90b5e3ff201ec024e4df7d87f009
Lucas Felici <lucas.felici@mercadolibre.com>
2021-12-29 15:21:58 -0300

    Binding activated, MainActivity binding setup

--- app/src/main/java/com/example/borredapp/ui/component/main/MainActivity.kt
+++ app/src/main/java/com/example/borredapp/ui/component/main/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.borredapp.ui.component.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.borredapp.R
+import com.example.borredapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        this.title="Borred App"
     }
 }
