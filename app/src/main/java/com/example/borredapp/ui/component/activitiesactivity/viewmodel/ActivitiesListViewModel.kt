e2f3239aa0aed3606ad31227ab9985bca639c9f3
Lucas Felici <lucas.felici@mercadolibre.com>
2021-12-29 13:57:55 -0300

    acc test

--- app/src/main/java/com/example/borredapp/ui/component/activitiesactivity/viewmodel/ActivitiesListViewModel.kt
+++ app/src/main/java/com/example/borredapp/ui/component/activitiesactivity/viewmodel/ActivitiesListViewModel.kt
@@ -3,5 +3,5 @@ package com.example.borredapp.ui.component.activitiesactivity.viewmodel
 import androidx.lifecycle.ViewModel
 
 class ActivitiesListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
