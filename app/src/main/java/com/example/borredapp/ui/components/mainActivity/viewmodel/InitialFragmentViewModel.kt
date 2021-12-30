c115639290626177c4ea8559d84bc38513f39230
Lucas Felici <lucas.felici@mercadolibre.com>
2021-12-30 11:49:43 -0300

    Last changes in initial interface, minor changes in activity activities

--- app/src/main/java/com/example/borredapp/ui/components/mainActivity/viewmodel/InitialFragmentViewModel.kt
+++ app/src/main/java/com/example/borredapp/ui/components/mainActivity/viewmodel/InitialFragmentViewModel.kt
@@ -3,5 +3,7 @@ package com.example.borredapp.ui.components.mainActivity.viewmodel
 import androidx.lifecycle.ViewModel
 
 class InitialFragmentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    //fun
+    //Para mi este viewmodel y el de conditions estan de mas
 }
