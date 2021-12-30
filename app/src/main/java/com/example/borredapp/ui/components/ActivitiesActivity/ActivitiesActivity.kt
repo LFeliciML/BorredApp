a904f88c4879c8e2aa4d6b21df835301dd259b18
Lucas Felici <lucas.felici@mercadolibre.com>
2021-12-29 21:00:51 -0300

    Initial fragment interface done, and some other minor changes

--- app/src/main/java/com/example/borredapp/ui/components/ActivitiesActivity/ActivitiesActivity.kt
+++ app/src/main/java/com/example/borredapp/ui/components/ActivitiesActivity/ActivitiesActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.borredapp.R
 import com.example.borredapp.databinding.ActivityActivitiesBinding
+import com.example.borredapp.databinding.ActivityMainBinding
+import com.example.borredapp.ui.components.ActivitiesActivity.fragments.ListActivitiesFragment
+import com.example.borredapp.ui.components.mainActivity.fragments.InitialFragment
 
 class ActivitiesActivity : AppCompatActivity() {
 
@@ -13,6 +16,11 @@ class ActivitiesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityActivitiesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerViewActivities, ListActivitiesFragment())
+            .commit()
+
 
     }
 }
