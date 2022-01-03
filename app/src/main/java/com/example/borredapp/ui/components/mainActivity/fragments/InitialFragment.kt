47d5a69930d7c637d885bb34ea29ee93fe702ad7
Mauro Alderete <mauro.alderete@mercadolibre.com>
2022-01-03 18:02:36 -0300

    add documentation

--- app/src/main/java/com/example/borredapp/ui/components/mainActivity/fragments/InitialFragment.kt
+++ app/src/main/java/com/example/borredapp/ui/components/mainActivity/fragments/InitialFragment.kt
@@ -16,85 +16,91 @@ import com.example.borredapp.utils.BorredApp.Companion.prefs
 
 class InitialFragment : Fragment() {
 
-    private val viewModel : InitialFragmentViewModel by viewModels()
+    private val viewModel: InitialFragmentViewModel by viewModels()
     private lateinit var binding: InitialFragmentBinding
 
 
-    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = InitialFragmentBinding.inflate(layoutInflater)
-        binding.buttonStart.setOnClickListener{
 
-            if(conditionsAprobed()){
-                val insertUser = binding.etNumberPersons.text.toString()
+        //Event button Start
+        binding.buttonStart.setOnClickListener {
 
-            if (insertUser.isNullOrBlank()){
+            if (conditionsAprobed()) {
+                val insertValueUser = binding.etNumberPersons.text.toString()
+
+                if (insertValueUser.isNullOrBlank()) {
                     setSharedPreferences(0)
                     runActivity()
-            } else {
-                if(checkIsNumber(insertUser)){
-                    runActivity()
-                } else{
-                    Toast.makeText(this.context,
-                    "Los numeros deben ser mayores o iguales a 0.",
-                    Toast.LENGTH_SHORT).show()
+                } else {
+                    if (checkIsNumber(insertValueUser)) {
+                        runActivity()
+                    } else {
+                        Toast.makeText(
+                            this.context,
+                            "Los numeros deben ser mayores o iguales a 0.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
             } else {
-                Toast.makeText(this.context,"Aceptar términos y condiciones",
-                Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this.context, "Aceptar términos y condiciones",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
-       binding.linkConditions.setOnClickListener {
-          parentFragmentManager.beginTransaction()
-              .add(R.id.fragmentContainerView,ConditionsFragment()).addToBackStack(null).commit()
-       }
+        binding.linkConditions.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView, ConditionsFragment()).addToBackStack(null).commit()
+        }
 
         return binding.root
     }
 
 
     //This function get sharedPreferences.
-    private fun setSharedPreferences(participants:Int){
+    private fun setSharedPreferences(participants: Int) {
         val editor = prefs.edit()
-        editor.putInt("PARTICIPANTS",participants)
+        editor.putInt("PARTICIPANTS", participants)
         editor.commit()
     }
 
     //This execute activities activity.
-    private fun runActivity(){
+    private fun runActivity() {
         val intent = Intent(this.context, ActivitiesActivity::class.java)
         startActivity(intent)
     }
 
     //This function tries to convert String to int,
     // if the conversion fails, it throws an exception.
-    private fun checkIsNumber(number : String) : Boolean{
+    private fun checkIsNumber(number: String): Boolean {
         try {
-            var participants = number.toInt()
-            if (participants>=0){
-                setSharedPreferences(participants)
-               return true
+            val participants = number.toInt() //If it cannot be converted, the exception is thrown at this point.
+            if (participants >= 0) {
+                setSharedPreferences(participants) //set shared preferences.
+                return true
             }
             return false
-        }catch (e:NumberFormatException){
-            Toast.makeText(this.context,
-            "Solo se permite ingresar numeros enteros.",
-            Toast.LENGTH_SHORT).show()
+        } catch (e: NumberFormatException) {
+            Toast.makeText(
+                this.context,
+                "Solo se permite ingresar numeros enteros.",
+                Toast.LENGTH_SHORT
+            ).show()
             return false
         }
     }
 
 
-    private fun conditionsAprobed():Boolean{
-       return binding.checkBoxConditions.isChecked
+    //This function checks that the terms and conditions have been approved
+    private fun conditionsAprobed(): Boolean {
+        return binding.checkBoxConditions.isChecked
     }
-
-
 
 
 }
