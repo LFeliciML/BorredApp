3172230e5d5045a1aad93861749350f113332fc6
Mauro Alderete <mauro.alderete@mercadolibre.com>
2022-01-04 10:14:11 -0300

    optimizing project

--- app/src/main/java/com/example/borredapp/ui/components/mainActivity/viewmodel/ConditionsFragmentViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.borredapp.ui.components.mainActivity.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class ConditionsFragmentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
